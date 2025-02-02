737b58d56aa6b0c7e9af6894fd40864cf8afa62e
Mohamed Adel <malakmazher536@gmail.com>
2025-02-02 19:05:58 +0200

    last

--- src/EmptyPage.jsx
+++ src/EmptyPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const EmptyPage = () => {
   return (
-    // An empty page; you can add styles if you need full-screen blank appearance
     <div style={{ height: "100vh" }}></div>
   );
 };
